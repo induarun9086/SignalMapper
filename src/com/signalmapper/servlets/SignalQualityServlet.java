62a9f6f201250e3ef9e40cf70d243ba711ae0c2b
Indumathi Dinesh Arun <indu9086@gmail.com>
2014-11-05 11:20:19 +0100

    Change method to POST for getSignalQuality

--- src/com/signalmapper/servlets/SignalQualityServlet.java
+++ src/com/signalmapper/servlets/SignalQualityServlet.java
@@ -16,7 +16,7 @@ public class SignalQualityServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doGet(HttpServletRequest request,
+	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
 		String numPoints = request.getParameter("numPoints");
