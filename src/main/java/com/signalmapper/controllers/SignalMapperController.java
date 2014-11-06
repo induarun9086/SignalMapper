a870ff2c45c2b1ba3be4bb77820740e1f082b78a
Indumathi Dinesh Arun <indu9086@gmail.com>
2014-11-06 09:39:16 +0100

    Added spring controller for getSignalQuality

--- src/main/java/com/signalmapper/controllers/SignalMapperController.java
+++ src/main/java/com/signalmapper/controllers/SignalMapperController.java
@@ -1,44 +1,49 @@
-package com.signalmapper.servlets;
+package com.signalmapper.controllers;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.signalmapper.util.StringUtil;
 
-public class SignalQualityServlet extends HttpServlet {
+@RestController
+@RequestMapping("/getSignalData")
+public class SignalMapperController {
 
-	private static final long serialVersionUID = 1L;
-
-	@Override
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-
-		String numPoints = request.getParameter("numPoints");
-
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody
+	void generateReport(HttpServletRequest request,
+			HttpServletResponse response,
+			@RequestParam("numPoints") String numPoints) throws IOException {
+		
 		if (StringUtil.isNullOrEmpty(numPoints)) {
 			throw new IOException("The field numPoints cannot be empty or null");
 		}
 
 		Random randomGenerator = new Random();
-		
+
 		ObjectMapper mapper = new ObjectMapper();
-		
+
 		List<Integer> list = new ArrayList<Integer>();
-		
+
 		for (int i = 0; i < Integer.valueOf(numPoints); i++) {
 			int randomInt = randomGenerator.nextInt(100);
 			list.add(randomInt);
 		}
-		
+
 		mapper.writeValue(response.getWriter(), list);
 
 	}
+
 }
