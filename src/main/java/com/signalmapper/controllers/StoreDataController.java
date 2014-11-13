58d2b6b7305fb9e078db1f1aff397caa511a8237
Indumathi Dinesh Arun <indu9086@gmail.com>
2014-11-13 15:49:56 +0100

    Datanucleus maven plugin added

--- src/main/java/com/signalmapper/controllers/StoreDataController.java
+++ src/main/java/com/signalmapper/controllers/StoreDataController.java
@@ -3,8 +3,6 @@ package com.signalmapper.controllers;
 import java.io.IOException;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,22 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.signalmapper.data.model.Technology;
 import com.signalmapper.input.SignalStrengthData;
+import com.signalmapper.util.EMFService;
 
 @RestController
 public class StoreDataController {
-	
-	@RequestMapping(value="/signalstrength",method = RequestMethod.POST)
-	
+
+	@RequestMapping(value = "/signalstrength", method = RequestMethod.POST)
 	public @ResponseBody
-	void recordSignalStrength(@RequestBody SignalStrengthData signalStrengthData)  throws IOException {
-		EntityManagerFactory entityManagerFactory =  Persistence
-	            .createEntityManagerFactory("transactions-optional");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
+	void recordSignalStrength(@RequestBody SignalStrengthData signalStrengthData)
+			throws IOException {
+
+		EntityManager em = EMFService.get().createEntityManager();
 		Technology technology = new Technology();
 		technology.setName(signalStrengthData.getTechnology());
-		entityManager.getTransaction().commit();
-		entityManager.close();		
+		em.persist(technology);
+		System.out.println("Entry inserted successfully");
 	}
 
 }
